9e76de7cfff41afda9355de38a228adafde9946e
Alexander Kochergin <alexander.kochergin@aboutyou.de>
2016-07-19 16:22:47 +0200

    wip

--- config/nvim/settings/neomake.vim
+++ config/nvim/settings/neomake.vim
@@ -1,4 +1,4 @@
 let g:neomake_javascript_enabled_makers = ['eslint']
-let g:neomake_open_list = 1
+" let g:neomake_open_list = 1
 
 autocmd BufWritePost * Neomake
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,23 @@
 alias reload!='source ~/.zshrc'
 
 alias vim="nvim"
+
+# Filesystem aliases
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+# Go to dotfiles
+alias dotfiles="cd ~/.dotfiles"
+
+# npm
+alias ni='npm install'
+alias nrd='npm run dev'
+
+# Tmux
+alias ta='tmux attach -t'
+alias tkss='tmux kill-session -t'
+alias tksv='tmux kill-server'
+alias tl='tmux list-sessions'
+alias ts='tmux new-session -s'
